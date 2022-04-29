9281aaa64d1416deea59944161c0915168b8731c
huangzw <huangzhiwen220211@credithc.com>
2022-04-29 19:21:40 +0800

    init

---
+++ static/welcome.3229dc17.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.cfaaa3f9.js";import{o as r,f as o,g as t}from"./vendor.dc2a18cc.js";const _={},n=t("iframe",{src:"https://animpen.com/run/pen/quZQsu",frameborder:"0"},null,-1),c=[n];function s(a,l){return r(),o("div",null,c)}var i=e(_,[["render",s]]);export{i as default};
--- static/welcome.a35f44fd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.df05901a.js";import{o as r,f as o,g as t}from"./vendor.dc2a18cc.js";const _={},n=t("iframe",{src:"https://animpen.com/run/pen/quZQsu",frameborder:"0"},null,-1),c=[n];function s(a,l){return r(),o("div",null,c)}var i=e(_,[["render",s]]);export{i as default};
